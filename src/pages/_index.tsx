b6a7cb0bb1b9595508cb3f819f73269ca3bdc295
Viktor Bostrand <viktor.bostrand@gmail.com>
2024-08-28 10:44:30 +0200

    fix: disable suspense by default in example page

--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -1,26 +1,23 @@
-import React, { Suspense } from "react";
+import React from "react";
 import {
   Header,
   PullRequests,
   Issues,
   Repositories,
   Footer,
-} from "src/features/dashboard-suspended";
+} from "src/features/dashboard";
 import { Layout } from "src/components/Layout";
-import { SuspendedDashboardSkeleton } from "src/features/dashboard-suspended/skeleton";
 
 export const DashboardRoute: React.FC = () => {
   return (
-    <Suspense fallback={<SuspendedDashboardSkeleton />}>
-      <>
-        <Header />
-        <Layout>
-          <PullRequests />
-          <Issues />
-          <Repositories />
-        </Layout>
-        <Footer />
-      </>
-    </Suspense>
+    <>
+      <Header />
+      <Layout>
+        <PullRequests />
+        <Issues />
+        <Repositories />
+      </Layout>
+      <Footer />
+    </>
   );
 };
